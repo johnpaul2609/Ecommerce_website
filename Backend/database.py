bd0b54aee818c360bee9d8f597f0742ca5d91fc6
Johnpaul <johnjoshva814@gmail.com>
2026-06-08 20:38:05 +0530

    Initial project structure

---
+++ Backend/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "mysql+pymysql://root:johnpaul%40123@localhost/ecommerce_db"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
