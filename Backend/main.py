a24d560b02279b58fda756177604049f831182c0
Johnpaul <johnjoshva814@gmail.com>
2026-07-05 13:19:05 +0530

    update cors origins

--- Backend/main.py
+++ Backend/main.py
@@ -22,7 +22,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://ecommerce-website-pvuu.vercel.app"
+        "https://ecommerce-website-pvuu.vercel.app",
+        "https://ecommerce-website-e8py.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
